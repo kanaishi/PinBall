d310a94c4535ed565b2cf5ae2235af32b2df71ee
kanaishi_hitoshi <sapporokyorokyoro@gmail.com>
2018-10-15 11:16:36 +0900

    Lesson5課題提出
    課題と発展課題の提出です。発展課題は途中で行き詰りました。
    左右同時に押されたか？の考え方が分からなかったです。
    宜しくお願いします。

---
+++ Assets/BallController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    public GameObject TextScore; //テキストスコア表示
+    public int score;
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 2;
+            TextScore.GetComponent<Text> ().text = score.ToString ();
+
+            //Debug.Log("SmallStarTag" + other.gameObject.name);
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 100;
+            TextScore.GetComponent<Text>().text = score.ToString();
+            Debug.Log("LargeStarTag" + other.gameObject.name);
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 50;
+            TextScore.GetComponent<Text>().text = score.ToString();
+           // Debug.Log("SmallCloudTag" + other.gameObject.name);
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 1000;
+            TextScore.GetComponent<Text>().text = score.ToString();
+            //Debug.Log("LargeCloudTag" + other.gameObject.name);
+            //Debug.Log("Hit" + other.gameObject.name);
+        }
+
+       
+    }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            var id = Input.touches[i].fingerId;
+            var pos = Input.touches[i].position;
+            Debug.LogFormat("{0} - x:{1}, y:{2}", id, pos.x, pos.y);
+            if (id == 0)
+            {
+                //指１本の場合
+                if ((pos.x <= 250) && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+                else if((pos.x >=250) && tag == "RightFripperTag")
+                {
+                    SetAngle(this.flickAngle);
+                }
+            }
+            else
+            {
+                //同時に押されたか？
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                SetAngle(this.defaultAngle);
+                //Debug.LogFormat("{0}:いま離された", id);
+                //指１本の場合
+                if ((pos.x <= 250) && tag == "LeftFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+                else if ((pos.x >= 250) && tag == "RightFripperTag")
+                {
+                    SetAngle(this.defaultAngle);
+                }
+            }
+                   
+
+
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
